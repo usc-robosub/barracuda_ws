a4706bf1a3f4947a5891fd8fb40e2d6ba50c4aa5
mihir <mihirsin@usc.edu>
2026-03-05 16:32:13 -0800

    added thruster manager submodule, launch file entry

--- src/barracuda_onboard/launch/barracuda_onboard.launch.py
+++ src/barracuda_onboard/launch/barracuda_onboard.launch.py
@@ -6,6 +6,30 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
+thruster_manager_params = {
+    "tam.thrusters": [
+        "barracuda/thruster_0_joint",
+        "barracuda/thruster_1_joint",
+        "barracuda/thruster_2_joint",
+        "barracuda/thruster_3_joint",
+        "barracuda/thruster_4_joint",
+        "barracuda/thruster_5_joint",
+        "barracuda/thruster_6_joint",
+        "barracuda/thruster_7_joint",
+    ],
+    "tam.min_thrust": -4.0,
+    "tam.max_thrust": 4.0,
+    "control_frame": "barracuda/base_link",
+}
+# create thruster_manager node
+node_thruster_manager = Node(
+    package="thruster_manager",
+    namespace="barracuda",
+    executable="thruster_manager_node",
+    output="screen",
+    parameters=[thruster_manager_params],
+)
+
 
 def include_launch_description(package, launchfile):
     return IncludeLaunchDescription(
@@ -25,7 +49,8 @@ def generate_launch_description():
             [
                 include_launch_description(
                     "foxglove_bridge", "foxglove_bridge_launch.xml"
-                )
+                ),
+                node_thruster_manager,
             ]
         )
     elif getenv("PLATFORM") == "pi":
@@ -34,6 +59,8 @@ def generate_launch_description():
                 include_launch_description(
                     "foxglove_bridge", "foxglove_bridge_launch.xml"
                 ),
-                include_launch_description("barracuda_thrusters", "barracuda_thrusters_launch.py")
+                include_launch_description(
+                    "barracuda_thrusters", "barracuda_thrusters_launch.py"
+                ),
             ]
         )
