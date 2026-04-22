22820b582bc30a77b2bb70fa1a9510d007e0ad10
mihir <mihirsin@usc.edu>
2026-04-22 16:02:03 -0700

    bring back comments in barracuda_onboard.launch.py

--- src/barracuda_onboard/launch/barracuda_onboard.launch.py
+++ src/barracuda_onboard/launch/barracuda_onboard.launch.py
@@ -3,12 +3,18 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.logging import get_logger
+
+
+
+
 
 logger = get_logger("barracuda_onboard.launch.py")
 
 
+# helper to include a given external launch file
 def include_launch_description(package, launchfile):
     return IncludeLaunchDescription(
         PathJoinSubstitution(
@@ -21,28 +27,28 @@ def include_launch_description(package, launchfile):
     )
 
 
-src_root = "/root/barracuda_ws/src"
-pkgs = [
-    pkg
-    for pkg in os.listdir(src_root)
-    if pkg != "barracuda_onboard"
-    and os.path.isfile(os.path.join(src_root, pkg, "package.xml"))
-]
+# scan workspace src dir, make a list of all package names except for barracuda_onboard
+pkgs = [pkg for pkg in os.listdir("/root/barracuda_ws/src") if pkg != "barracuda_onboard"]
 
+
+# if PKG_SEL env var is set, only include packages specified by PKG_SEL in pkg list
 if selected_pkgs_str := os.getenv("PKG_SEL"):
-    selected_pkgs = [
-        pkg for pkg in selected_pkgs_str.split(" ") if pkg and pkg != "barracuda_onboard"
-    ]
-    pkgs = [pkg for pkg in selected_pkgs if pkg in pkgs]
+    pkgs = [pkg for pkg in selected_pkgs_str.split(" ") if pkg in pkgs]
 
-    if selected_pkgs and not pkgs:
-        logger.warn("selected package(s) don't exist")
-
+    # show a warning if the pkg list ends up being empty (this could happen if invalid packages are specified in PKG_SEL)
+    if not pkgs:
+        logger.warn("selected package(s) don't exist") 
+     
+# for packages in barracuda_ws/src, naming convention is pkg_name.launch.py, so it's easy to include all of them in the
+# list below - this way we don't have to update this launch file (barracuda_onboard.launch.py) manually every time a new
+# package is added to barracuda_ws/src
 launch_inclusion_list = [include_launch_description(pkg, f"{pkg}.launch.py") for pkg in pkgs]
 
 
 def generate_launch_description():
     return LaunchDescription(
-        [include_launch_description("foxglove_bridge", "foxglove_bridge_launch.xml")]
-        + launch_inclusion_list
-    )
+
+        # always launch foxglove bridge
+        [include_launch_description("foxglove_bridge", "foxglove_bridge_launch.xml")] + launch_inclusion_list
+   )
+
