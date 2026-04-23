54cbb244429c67ac91decd6dac1f8eb2eee42e27
mihir <mihirsin@usc.edu>
2026-04-23 14:16:10 -0700

    create dedicated dependency-pkgs dir in src for packages without launchfiles to be used directly in barracuda_onboard_launch.py
    - skip zed_debug in Dockerfile
    - use --packages-up-to instead of --packages-select to build
    dependencies of PKG_SEL packages automatically
    - add check for launch dir in barracuda_onboard_launch.py
    - add dependency_pkgs dir in src

--- src/barracuda_onboard/launch/barracuda_onboard.launch.py
+++ src/barracuda_onboard/launch/barracuda_onboard.launch.py
@@ -8,10 +8,8 @@ from launch_ros.substitutions import FindPackageShare
 from launch.logging import get_logger
 
 
-
-
-
 logger = get_logger("barracuda_onboard.launch.py")
+workspace_src = "/root/barracuda_ws/src"
 
 
 # helper to include a given external launch file
@@ -28,7 +26,12 @@ def include_launch_description(package, launchfile):
 
 
 # scan workspace src dir, make a list of all package names except for barracuda_onboard
-pkgs = [pkg for pkg in os.listdir("/root/barracuda_ws/src") if pkg != "barracuda_onboard"]
+pkgs = [
+    pkg
+    for pkg in os.listdir(workspace_src)
+    if os.path.isdir(os.path.join(workspace_src, pkg, "launch"))
+    and pkg != "barracuda_onboard"
+]
 
 
 # if PKG_SEL env var is set, only include packages specified by PKG_SEL in pkg list
@@ -37,18 +40,19 @@ if selected_pkgs_str := os.getenv("PKG_SEL"):
 
     # show a warning if the pkg list ends up being empty (this could happen if invalid packages are specified in PKG_SEL)
     if not pkgs:
-        logger.warn("selected package(s) don't exist") 
-     
+        logger.warn("selected package(s) don't exist")
+
 # for packages in barracuda_ws/src, naming convention is pkg_name.launch.py, so it's easy to include all of them in the
 # list below - this way we don't have to update this launch file (barracuda_onboard.launch.py) manually every time a new
 # package is added to barracuda_ws/src
-launch_inclusion_list = [include_launch_description(pkg, f"{pkg}.launch.py") for pkg in pkgs]
+launch_inclusion_list = [
+    include_launch_description(pkg, f"{pkg}.launch.py") for pkg in pkgs
+]
 
 
 def generate_launch_description():
     return LaunchDescription(
-
         # always launch foxglove bridge
-        [include_launch_description("foxglove_bridge", "foxglove_bridge_launch.xml")] + launch_inclusion_list
-   )
-
+        [include_launch_description("foxglove_bridge", "foxglove_bridge_launch.xml")]
+        + launch_inclusion_list
+    )
